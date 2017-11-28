15b39e25d75ed01a615c8ef7a1c15694ce4a6138
THcodr63 <tnhami63@gmail.com>
2017-11-28 09:45:20 -0500

    update/Add

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,14 @@ $(document).ready(function() {
 
 $(".button-collapse").sideNav();
 
+
+$('.carousel').carousel({
+  padding: 200    
+});
+autoplay()   
+function autoplay() {
+  $('.carousel').carousel('next');
+  setTimeout(autoplay, 3000);
+}
+
+
